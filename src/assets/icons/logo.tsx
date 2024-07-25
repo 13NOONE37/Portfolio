1c5d85d1c3e4938a55497ebaadb905b66753ac77
13NOONE37 <oliwerklauze22@gmail.com>
2024-07-25 23:01:46 +0200

    logo added

---
+++ src/assets/icons/logo.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <svg
+      width="131"
+      height="26"
+      viewBox="0 0 131 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M99.8073 20.34H110.68V25.272H92.8242V20.34L103.622 4.932H92.8242V0H110.68V4.932L99.8073 20.34Z"
+        fill="white"
+      />
+      <path
+        d="M73.8123 0V10.17V20.34H82.6683V0H88.8242V25.2713H67.6562V0H73.8123Z"
+        fill="white"
+      />
+      <path
+        d="M44.6843 25.272H38.2402L47.3843 0H54.5123L63.6563 25.272H57.1403L50.9123 6.804L44.6843 25.272Z"
+        fill="white"
+      />
+      <path d="M26.176 20.34H34.24V25.272H20.02V0H26.176V20.34Z" fill="white" />
+      <path
+        d="M120.836 4.932V20.34H130.16V25.272H114.68V0H130.16V4.932H120.836Z"
+        fill="white"
+      />
+      <path
+        d="M8.496 25.272L0 14.112V11.088L8.424 0H15.66L5.868 12.384L16.02 25.272H8.496Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+
+export default Logo;
