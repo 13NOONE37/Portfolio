40ed613285137aeefbc498e72f4b6481d9bcf438
13NOONE37 <oliwerklauze22@gmail.com>
2024-09-22 20:38:47 +0200

    phone number update

--- src/config/personalInformations.tsx
+++ src/config/personalInformations.tsx
@@ -1,6 +1,6 @@
 export const personalInformations = {
   email: 'oklauze.kontakt@gmail.com',
-  phone: '+48 700 421 133',
+  phone: '+48 455 443 483',
   github: 'https://github.com/13NOONE37',
   messenger: 'https://www.messenger.com/t/100007303174533',
 };
