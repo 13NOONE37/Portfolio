9c0e2d18594e06b8b3936282e239bbcc1bd645aa
13NOONE37 <oliwerklauze22@gmail.com>
2024-09-23 10:27:27 +0200

    not found page

--- src/app/[locale]/[...rest]/not-found.tsx
+++ src/app/[locale]/[...rest]/not-found.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './not-found.module.css';
+import fonts from '@/styles/fonts.module.css';
+import { useTranslations } from 'next-intl';
+import { PrimaryLink } from '@/components/buttons/primary/primary';
 
 const NotFoundPage = () => {
-  return <div>Rest not found</div>;
+  const t = useTranslations('Shared');
+  return (
+    <section className={styles.container}>
+      <h1 className={fonts.heading_600}>{t('not_found')}</h1>
+      <div>
+        <PrimaryLink href="/">{t('go_back_home_page')}</PrimaryLink>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundPage;
